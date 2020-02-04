bc9f65b79727f4c7cdfde82ae5997fc924e337fa
France Julien <julien@xaop.com>
2020-02-04 12:54:25 +0100

    packaged

--- src/find-options/FindConditions.ts
+++
@@ -1,8 +0,0 @@
-import {FindOperator} from "./FindOperator";
-
-/**
- * Used for find operations.
- */
-export type FindConditions<T> = {
-    [P in keyof T]?: T[P] extends never ? FindConditions<T[P]>|FindOperator<FindConditions<T[P]>> : FindConditions<T[P]>|FindOperator<FindConditions<T[P]>>;
-};
--- test/github-issues/2693/entity/user.ts
+++
@@ -1,8 +0,0 @@
-import {PrimaryGeneratedColumn} from "../../../../src/decorator/columns/PrimaryGeneratedColumn";
-import {Entity} from "../../../../src/decorator/entity/Entity";
-
-@Entity({name: "users", synchronize: false})
-export class User {
-    @PrimaryGeneratedColumn("uuid")
-    id: number;
-}
