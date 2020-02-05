3838f7098a08d0ffe5cdfb92c6b67bcb478c8eac
France Julien <julien@xaop.com>
2020-02-05 16:25:18 +0100

    packaged

--- test/github-issues/3857/entity/Women.ts
+++
@@ -1,9 +0,0 @@
-import {Person} from "./Person";
-import {ChildEntity, Column} from "../../../../src";
-
-@ChildEntity()
-export class Women extends Person {
-
-    @Column("int")
-    brassiereSize: number;
-}
